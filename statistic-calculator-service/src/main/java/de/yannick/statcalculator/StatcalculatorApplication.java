18eeb6a7fcbfde0ad640973f9a05fe29f9907768
Yannick Epstein <yannick.epstein@gmail.com>
2020-08-15 13:19:33 +0200

    Initial Commit For Statistic Calculator Service
    Added Spring Boot project with the Statistic Calculator Webservice

---
+++ statistic-calculator-service/src/main/java/de/yannick/statcalculator/StatcalculatorApplication.java
@@ -0,0 +1,13 @@
+package de.yannick.statcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatcalculatorApplication.class, args);
+	}
+
+}
---
+++ statistic-calculator-service/src/test/java/de/yannick/statcalculator/StatcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.yannick.statcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
